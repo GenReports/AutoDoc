e9b03f26f2bd143ab01916d0e20a4c5a9b091026
Eduardo Rezende <dudumoises2005@gmail.com>
2025-08-13 19:34:43 -0300

    ✨feat: Add IEnumerableExtensions
    Adding an extension class for IEnumerable with a custom Chunk method

---
+++ src/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace AutoDoc.Extensions
+{
+    internal static class IEnumerableExtensions
+    {
+        public static IEnumerable<T[]> Chunk<T>(this IEnumerable<T> values)
+        {
+            if (values is null || !values.Any())
+                return [];
+
+            var totalCount = values.Count();
+
+            if (totalCount < 3)
+                return [[.. values]];
+
+            return totalCount < 10
+                ? values.Chunk(totalCount / 3)
+                : values.Chunk(totalCount / 5);
+        }
+    }
+}

64f5aeb99f42f28e13c891599d5b38ebe3c81849
Eduardo Rezende <dudumoises2005@gmail.com>
2025-08-13 19:35:29 -0300

    ✨feat: Add TaskExtensions
    Adding an extension class for Task with a custom Delay method

---
+++ src/Extensions/TaskExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace AutoDoc.Extensions
+{
+    internal static class TaskExtensions
+    {
+        public static async Task DelayAsync(
+            this int count,
+            int multiplier = 1000,
+            CancellationToken ct = default)
+        {
+            await Task.Delay(count * multiplier, ct);
+        }
+    }
+}

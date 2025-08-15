8dea33a642ea46500223f11a06803f4bfbc25b1a
Eduardo Rezende <dudumoises2005@gmail.com>
2025-08-14 22:48:06 -0300

    ✨feat: Add new IEnumerableExtensions
    Adding extension method for Setting value

--- src/Extensions/IEnumerableExtensions.cs
+++ src/Extensions/IEnumerableExtensions.cs
@@ -1,8 +1,11 @@
-﻿namespace AutoDoc.Extensions
+﻿using System.Reflection;
+using System.Linq.Expressions;
+
+namespace AutoDoc.Extensions
 {
     internal static class IEnumerableExtensions
     {
-        public static IEnumerable<T[]> Chunk<T>(this IEnumerable<T> values)
+        public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> values)
         {
             if (values is null || !values.Any())
                 return [];
@@ -15,6 +18,26 @@
             return totalCount < 10
                 ? values.Chunk(totalCount / 3)
                 : values.Chunk(totalCount / 5);
+        }
+
+        public static IEnumerable<T> Set<T, TProperty>(
+            this IEnumerable<T> contents,
+            Expression<Func<T, TProperty>> expression,
+            TProperty value)
+        {
+            if (contents is null || !contents.Any())
+                return [];
+
+            if (expression.Body is not MemberExpression memberExpr)
+                return [];
+
+            if (memberExpr.Member is not PropertyInfo propInfo)
+                return [];
+
+            foreach (var content in contents)
+                propInfo.SetValue(content, value);
+
+            return contents;
         }
     }
 }

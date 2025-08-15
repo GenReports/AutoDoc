c826a7b014a507e5d5380127786884f1593a00f6
Eduardo Rezende <dudumoises2005@gmail.com>
2025-08-14 22:41:26 -0300

    ✨feat: Add AppSettings class
    Adding class to represent appsettings

---
+++ src/Models/AppSettings.cs
@@ -0,0 +1,31 @@
+﻿namespace AutoDoc.Models
+{
+    internal class AppSettings
+    {
+        public AppSettings()
+        {
+            RepositoryPath = string.Empty;
+            OwnerEmail = string.Empty;
+            OwnerName = string.Empty;
+            Culture = string.Empty;
+            OutputPath = string.Empty;
+            CompletionsUri = string.Empty;
+        }
+
+        public string RepositoryPath { get; set; }
+
+        public string OwnerEmail { get; set; }
+
+        public string OwnerName { get; set; }
+
+        public string Culture { get; set; }
+
+        public string OutputPath { get; set; }
+
+        public string CompletionsUri { get; set; }
+
+        public int DelayMillisecondsMultiplier { get; set; }
+
+        public double ModelTemperature { get; set; }
+    }
+}

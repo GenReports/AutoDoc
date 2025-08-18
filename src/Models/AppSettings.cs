07021b5662e5df179b84b24467888e13513ef1ea
Eduardo Rezende <dudumoises2005@gmail.com>
2025-08-18 17:36:34 -0300

    ♻️refactor: Update AppSettings
    Adding AuthenticationHeader and ApiKey fields

--- src/Models/AppSettings.cs
+++ src/Models/AppSettings.cs
@@ -12,6 +12,8 @@ namespace AutoDoc.Models
             Culture = string.Empty;
             OutputPath = string.Empty;
             CompletionsUri = string.Empty;
+            AuthenticationHeader = string.Empty;
+            ApiKey = string.Empty;
         }
 
         public string RepositoryPath { get; set; }
@@ -29,6 +31,10 @@ namespace AutoDoc.Models
         public string OutputPath { get; set; }
 
         public string CompletionsUri { get; set; }
+
+        public string AuthenticationHeader { get; set; }
+
+        public string ApiKey { get; set; }
 
         public int DelayMilliseconds { get; set; }
 
